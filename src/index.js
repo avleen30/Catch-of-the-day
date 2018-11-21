cc4295c3b9779393940d9a76a67dfcb2d3ae46f0
Avleen Dhaliwal <avleen.dhaliwal@shopify.com>
2018-11-20 20:42:44 -0500

    added routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
-import App from './components/App';
-import './css/style.css';
+import Router from './components/Router';
+import "./css/style.css"
 
 render(
-  <App />,
+  <Router />,
   document.getElementById('main')
 );
