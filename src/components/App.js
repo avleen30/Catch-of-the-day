1fe983370b0ba28e6fda85ada833e3bc3f1fd9fe
Avleen Dhaliwal <avleen.dhaliwal@shopify.com>
2018-11-27 14:48:44 -0500

    displaying Order state

--- src/components/App.js
+++ src/components/App.js
@@ -28,17 +28,38 @@ class App extends React.Component {
     });
   };
 
+  addToOrder = (key) => {
+    // 1. Take a copy of the setState
+    const order = {...this.state.order};
+    // 2. Either add to order or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. call setState toupdate our state object
+    this.setState({
+      order: order
+    })
+  };
+
   render(){
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}>{ key }</Fish>)}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}/>
+            ))}
           </ul>
         </div>
-          <Order />
-          <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+          <Order
+            fishes= {this.state.fishes}
+            order={this.state.order}/>
+          <Inventory
+            addFish={this.addFish}
+            loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { formatPrice } from '../helpers'
 
 class Fish extends React.Component {
-  render() {
-    const { image, name, desc, price, availability} = this.props.details;
 
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  }
+
+  render() {
+    const { image, name, desc, price, availability, status } = this.props.details;
+
+    const isAvailable = status === 'available';
 
     return (
       <li className="menu-fish">
@@ -14,7 +20,9 @@ class Fish extends React.Component {
         <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          { isAvailable ? "Add To Order": "Sold out" }
+        </button>
       </li>
     );
   }
